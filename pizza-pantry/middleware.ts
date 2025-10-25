8976f70d6a39beb993fff616b6b649daa68b1c75
Melusi19 <69846426+Melusi19@users.noreply.github.com>
2025-10-25 20:55:55 +0200

    middleware file

---
+++ pizza-pantry/middleware.ts
@@ -0,0 +1,20 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  publicRoutes: [
+    "/",                            
+    "/sign-in(.*)",                    
+    "/sign-up(.*)",                     
+    "/api/webhook/clerk",              
+  ],
+  ignoredRoutes: [
+    "/api/webhook/clerk",             
+  ],
+});
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
