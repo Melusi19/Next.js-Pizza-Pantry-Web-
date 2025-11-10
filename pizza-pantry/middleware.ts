c8723558b20a39d9238969a571e80ff774547f80
Melusi19 <69846426+Melusi19@users.noreply.github.com>
2025-11-10 17:34:48 +0200

    Replace authMiddleware with clerkMiddleware

--- pizza-pantry/middleware.ts
+++ pizza-pantry/middleware.ts
@@ -1,20 +1,10 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default authMiddleware({
-  publicRoutes: [
-    "/",                            
-    "/sign-in(.*)",                    
-    "/sign-up(.*)",                     
-    "/api/webhook/clerk",              
-  ],
-  ignoredRoutes: [
-    "/api/webhook/clerk",             
-  ],
-});
+export default clerkMiddleware()
 
 export const config = {
   matcher: [
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    "/(api|trpc)(.*)",
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    '/(api|trpc)(.*)',
   ],
-};
+}
